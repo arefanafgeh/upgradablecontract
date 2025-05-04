c6ae174220dc24a16549b821cf46c1e06a0e25bb
aref <aanafgeh@gmail.com>
2025-05-04 21:41:48 +0330

    First test of custome proxy upgradable contracts using hardhat done

--- test/firsttest.js
+++ test/firsttest.js
@@ -22,36 +22,36 @@ describe("manual Proxy test",function(){
     });
 
     it("Should set and get value via proxy", async function () {
-        const proxyAsLogicV1 = LogicV1.attach(await proxyInstance.getAddress());
-        console.log(await proxyAsLogicV1.interface.fragments);
+        const proxyAsLogicV1 = await ethers.getContractAt("LogicV1",await proxyInstance.getAddress());
+        // console.log(await proxyAsLogicV1.interface.fragments);
         await proxyAsLogicV1.setX(42);
-        console.error(await proxyAsLogicV1.getX());
-        expect(await proxyAsLogicV1.getX()).to.equal(42);
+        // console.error(await proxyAsLogicV1.x());
+        expect(await proxyAsLogicV1.x()).to.equal(42);
         await proxyAsLogicV1.increament();
-        expect(await proxyAsLogicV1.getX()).to.equal(43);
+        expect(await proxyAsLogicV1.x()).to.equal(43);
     });
 
+   it("Should upgrade to LogicV2 and retain state", async function () {
+        // Deploy upgraded logic contract
+        LogicV2 = await ethers.getContractFactory("LogicV2");
+        logicV2Instance = await LogicV2.deploy();
+        await logicV2Instance.waitForDeployment();
 
+        // Upgrade proxy to use LogicV2
+        await proxyInstance.upgrade(await logicV2Instance.getAddress());
+
+        // Attach proxy to new LogicV2 ABI
+        const proxyAsLogicV2 = await ethers.getContractAt("LogicV2",await proxyInstance.getAddress());
+        await proxyAsLogicV2.setX(42);
+        // Verify state persists
+        expect(await proxyAsLogicV2.x()).to.equal(42);
+
+        await proxyAsLogicV2.increament();
+        expect(await proxyAsLogicV2.x()).to.equal(52);
+    });
 
 })
 
 
 
-    // it("Should upgrade to LogicV2 and retain state", async function () {
-    //     // Deploy upgraded logic contract
-    //     LogicV2 = await ethers.getContractFactory("LogicV2");
-    //     logicV2Instance = await LogicV2.deploy();
-    //     await logicV2Instance.waitForDeployment();
-
-    //     // Upgrade proxy to use LogicV2
-    //     await proxyInstance.upgrade(logicV2Instance.address);
-
-    //     // Attach proxy to new LogicV2 ABI
-    //     const proxyAsLogicV2 = LogicV2.attach(proxyInstance.address);
-    //     await proxyAsLogicV1.setX(42);
-    //     // Verify state persists
-    //     expect(await proxyAsLogicV2.getValue()).to.equal(42);
-
-    //     await proxyAsLogicV1.increament();
-    //     expect(await proxyAsLogicV2.doubleValue()).to.equal(52);
-    // });
+ 
